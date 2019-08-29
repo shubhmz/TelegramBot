096bff3bd06a7bbed2e5b2e12b0edf7a66ae00fa
Priyam005 <43999863+Priyam005@users.noreply.github.com>
2019-08-29 21:46:58 +0530

    Update kang.py

--- stdplugins/kang.py
+++ stdplugins/kang.py
@@ -44,8 +44,8 @@ async def _(event):
 
     me = borg.me
     userid = event.from_id
-    packname = f"Priyam's Stickers Pack"
-    packshortname = f"Uniborg_Pack_{userid}"  # format: Uni_Borg_userid
+    packname = f"Priyam's Stickers Pack 2"
+    packshortname = f"Uniborg_Pack2_{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "@UniBorg_Sticker.png"
