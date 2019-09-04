87a9c10fd269a84529612c0edca4852077ffdf1b
Priyam005 <43999863+Priyam005@users.noreply.github.com>
2019-09-04 14:45:05 +0530

    Update pmpermit.py

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -15,15 +15,18 @@ borg.storage.PM_WARNS = {}
 borg.storage.PREV_REPLY_MESSAGE = {}
 
 
-BAALAJI_TG_USER_BOT = "My Master hasn't approved you to PM."
-TG_COMPANION_USER_BOT = "Please wait for his response and don't spam his PM."
-UNIBORG_USER_BOT_WARN_ZERO = "I am currently offline. Please do not SPAM me."
-UNIBORG_USER_BOT_NO_WARN = """
+BAALAJI_TG_USER_BOT = "```My Master hasn't approved you to PM.```"
+TG_COMPANION_USER_BOT = "```Please wait for his response and don't spam his PM.```"
+UNIBORG_USER_BOT_WARN_ZERO = "```I am currently offline. Please do not SPAM me.```"
+UNIBORG_USER_BOT_NO_WARN = """```
 Bleep blop! This is a bot. Don't fret.
+
 My master hasn't approved you to PM.
+
 Please wait for my master to look in, he mostly approves PMs.
+
 As far as I know, he doesn't usually approve retards though.
-"""
+```"""
 
 @borg.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def monito_p_m_s(event):
